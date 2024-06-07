641039415a198f5251ab6f1882b9ebb43a5e24ec
Panos Simatis <panos@parallelchain.io>
2024-06-07 13:03:40 +0800

    restricted moves to slots with pebbles, refactored

--- mancala.py
+++ mancala.py
@@ -1,102 +1,100 @@
 import random
 
+BANK = 5
+
 class Game:
     board = {}
     logic = {}
     
-    def __init__(this, logic):
-        this.board = {1: [5,5,5,5,5,0], 2: [5,5,5,5,5,0]}
-        this.logic = logic
+    def __init__(self, logic):
+        self.board = {1: [5,5,5,5,5,0], 2: [5,5,5,5,5,0]}
+        self.logic = logic
 
-    def side_empty(this):
-        for p in this.board:
-            if sum(this.board[p][:-1]) == 0:
-                return True
-        return False
+    def is_side_empty(self):
+        return any(sum(self.board[side][:BANK]) == 0 for side in self.board)
     
-    def slot_empty(this, p, s):
-        return True if this.board[p][s] == 0 else False
+    def is_slot_empty(self, side, idx):
+        return self.board[side][idx] == 0
     
-    def switch_side(this, p):
-        return 2 if p == 1 else 1
+    def switch_side(self, side):
+        return 2 if side == 1 else 1
     
-    def player_choice(this, p):
-        if this.logic[p] == 'random':
+    def player_choice(self, player):
+        if self.logic[player] == 'random':
             slot_picked = random.randint(0, 4)
-            while this.slot_empty(p, slot_picked):
+            while self.is_slot_empty(player, slot_picked):
                 slot_picked = random.randint(0, 4)
-        elif this.logic[p] == 'human':
-            slot_picked = int(input("Enter slot number: "))
+        elif self.logic[player] == 'human':
+            slot_picked = int(input("Enter slot number (0-4): "))
+            while slot_picked not in range(5) or self.is_slot_empty(player, slot_picked):
+                slot_picked = int(input("Invalid choice. Enter a slot number (0-4) that has pebbles: "))
         else:
-            # TODO AI
+            # TODO: Implement AI logic
             return 0
         return slot_picked
 
-    def move(this, s, idx):
-        player = s
-        steps = this.board[s][idx]
-        this.board[s][idx] = 0
-        while steps > 0:
-            idx += 1
-            if idx == 5:
-                if player == s:
-                    this.board[s][idx] += 1
-                else:
-                    steps += 1
-                idx = -1
-                s = this.switch_side(s)
+    def move(self, side, start_idx):
+        player = side
+        pebbles = self.board[side][start_idx]
+        self.board[side][start_idx] = 0
+        idx = start_idx
+        while pebbles > 0:
+            idx = (idx + 1) % (BANK + 1)
+            if idx == BANK:
+                if player == side:
+                    self.board[side][BANK] += 1
+                    pebbles -= 1
+                if pebbles > 0:
+                    idx = -1
+                    side = self.switch_side(side)
             else:
-                this.board[s][idx] += 1
-            steps -= 1
-        return {'side':s, 'idx':idx}
+                self.board[side][idx] += 1
+                pebbles -= 1
+        return {'side':side, 'idx':idx}
             
-    def capture(this, player, landing):
-        # Must land on current player's side
+    def capture(self, player, landing):
         if player != landing['side']:
-            return
-        
-        # Must land on an empty slot
-        if this.board[player][landing['idx']] > 1:
-            return
-        
-        # Slot on the other side must have pebbles
-        opponent = this.switch_side(player)
-        pebbles = this.board[opponent][landing['idx']]
+            return False
+        if self.board[player][landing['idx']] > 1:
+            return False
+        opponent = self.switch_side(player)
+        pebbles = self.board[opponent][landing['idx']]
         if pebbles > 0:
-            this.board[player][5] += pebbles + 1
-            this.board[opponent][landing['idx']] = 0
-            this.board[player][landing['idx']] = 0
-            print('Capture!')
+            self.board[player][BANK] += pebbles + 1
+            self.board[opponent][landing['idx']] = 0
+            self.board[player][landing['idx']] = 0
+            return True
+        return False
 
-    def check_bonus_round(this, landing):
-        return True if landing['idx'] == -1 else False
+    def check_bonus_round(self, landing):
+        return landing['idx'] == BANK
     
-    def get_winner(this):
-        if this.board[1][-1] > this.board[2][-1]:
-            return 'Player one won!'
-        elif this.board[1][-1] == this.board[2][-1]:
-            return 'Draw'
+    def get_winner(self):
+        if self.board[1][BANK] > self.board[2][BANK]:
+            return 1
+        elif self.board[1][BANK] == self.board[2][BANK]:
+            return 0
         else:
-            return 'Player two won!'
+            return 2
                 
-    def print_board(this):
-        print(this.board[1])
-        print(this.board[2])
+    def print_board(self):
+        print(self.board[1])
+        print(self.board[2])
         print('---------')
 
 def game_loop(players):
     game = Game(players)
     game.print_board()
-    while not game.side_empty():
-        p = 1
-        while p < 3:
-            slot_picked = game.player_choice(p)
-            print('Player', p, 'moves', slot_picked)
-            landing = game.move(p, slot_picked)
-            game.capture(p, landing)
+    while not game.is_side_empty():
+        player = 1
+        while player < 3:
+            slot_picked = game.player_choice(player)
+            print('Player', player, 'moves', slot_picked)
+            landing = game.move(player, slot_picked)
+            game.capture(player, landing)
             if game.check_bonus_round(landing):
                 print('Bonus round!')
             else:
-                p += 1
+                player += 1
             game.print_board()
     print(game.get_winner())
